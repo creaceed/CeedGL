71f65b62c5aa750d4f1061144ffac68941e3ce64
Raphael Sebbe <raphael@creaceed.com>
2015-02-21 13:49:07 +0100

    cross project dependencies update (using stub libs / implicit deps).

---
+++ stub.c
@@ -0,0 +1,11 @@
+//
+//  stub.c
+//  CeedBase
+//
+//  Created by Raphael Sebbe on 18/02/15.
+//  Copyright (c) 2015 Creaceed. All rights reserved.
+//
+
+__private_extern__ int __cr_stub() {
+	return 0;
+}
